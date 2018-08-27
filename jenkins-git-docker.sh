5df0403a1ce587e3883281ad20ca175bcf375be4
siwei <siweilxy@163.com>
2018-08-28 02:17:14 +0800

    add root for jenkins

--- jenkins-git-docker.sh
+++ jenkins-git-docker.sh
@@ -1,2 +1,2 @@
 sudo docker pull jenkins:latest
-#sudo docker run -t  -p 8080:8080 -p 8083:8083 -p 50000:50000  -v /home/siwei/jenkins_home:/home/jenkins_home jenkins
+#sudo docker run -t -u root -p 8080:8080 -p 8083:8083 -p 50000:50000  -v /home/siwei/jenkins_home:/var/jenkins_home jenkins
